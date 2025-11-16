6ce1640cd1911757abd2afd22bcf16f0d8fb5b4e
MuhIdrees22 <muhammad.idrees1@ontariotechu.net>
2025-11-16 15:36:21 -0500

    Create GroceryList.kt

---
+++ app/src/main/java/com/example/recipegroceryhelper/GroceryList.kt
@@ -0,0 +1,7 @@
+package com.example.recipegroceryhelper
+
+// A simple data class to represent a single grocery list.
+data class GroceryList(
+    val id: String = "",
+    val name: String = ""
+)
